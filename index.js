5450774684ac2bca34fee4bc9fc8446919539758
Babatunde <babatundea15@gmail.com>
2020-05-13 06:34:19 +0100

    i exported the server to a seperate file and installed middleware

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+// import the server
+const server = require("./server");
+// choose a port and set environment variables
+const port = process.env.PORT || 7000;
+
+
+// listen to the server
+server.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+// Imported needed middleware
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const server = express(); //Line 5 gives me access to req.body
 
-const server = express();
+// These are the helper functions
+const users = require("./users/userDb");
 
-server.get('/', (req, res) => {
+// Here is fleshing out a dummy API
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+// Here I am using my pre-baked middleware, Cors and helmet
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
 
 //custom middleware
 
 function logger(req, res, next) {}
 
+// export the server to another file
 module.exports = server;
