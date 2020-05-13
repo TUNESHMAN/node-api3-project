016e69eaba31c69f9b549a091c725cf753e84fc9
Babatunde <babatundea15@gmail.com>
2020-05-13 06:53:09 +0100

    I connected the router to the server

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+// Creating a Router
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -24,4 +25,5 @@ function validatePostId(req, res, next) {
   // do your magic!
 }
 
+// expose the router to the outer world
 module.exports = router;
--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const cors = require("cors");
 const helmet = require("helmet");
 const server = express(); //Line 5 gives me access to req.body
 
-// These are the helper functions
-const users = require("./users/userDb");
-
 // Here is fleshing out a dummy API
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+// We need to connect the server to the router so that the other endpoints can be accessed
+const userRouter = require("./users/userRouter");
+
 // Here I am using my pre-baked middleware, Cors and helmet
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
-
+server.use(userRouter);
 //custom middleware
 
 function logger(req, res, next) {}
