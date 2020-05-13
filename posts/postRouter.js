dcf8fa34e72fdbdac652f51b5e5502bdc0818146
Babatunde <babatundea15@gmail.com>
2020-05-13 11:44:44 +0100

    I fixed update post endpoint

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -40,10 +40,41 @@ router.get("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // do your magic!
+  // It needs the id of the post to be deleted
+  const { id } = req.params;
+  posts
+    .remove(id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json({ message: `The post ${id} has been deleted` });
+      } else {
+        res.status(404).json({ message: `No post with id ${id} exists` });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message, stack: error.stack });
+    });
 });
 
 router.put("/:id", (req, res) => {
   // do your magic!
+  // We need the id and the changes
+  const { id } = req.params;
+  const replacementPost = req.body;
+  posts
+    .update(id, replacementPost)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: `The post with id ${id} could not be found` });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message, stack: error.stack });
+    });
 });
 
 // custom middleware
